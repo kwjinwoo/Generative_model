2ddd53925f6e6f779a42af286858df37199f2e19
kwjinwoo <jung003327@gmail.com>
2024-12-24 23:01:02 +0900

    test: add autorgressive tests

--- src/gen_ai/models/autoregressive.py
+++ src/gen_ai/models/autoregressive.py
@@ -54,24 +54,22 @@ class MaskedConvResidualBlock(nn.Module):
         super().__init__()
 
         self.layers = nn.Sequential(
-            [
-                nn.Conv2d(in_channels=in_channels, out_channels=in_channels // 2, kernel_size=1, bias=False),
-                nn.BatchNorm2d(in_channels // 2),
-                nn.ReLU(),
-                MaskedConv2D(
-                    mask_type="B",
-                    in_channels=in_channels // 2,
-                    out_channels=in_channels // 2,
-                    kernel_size=3,
-                    padding=1,
-                    bias=False,
-                ),
-                nn.BatchNorm2d(in_channels // 2),
-                nn.ReLU(),
-                nn.Conv2d(in_channels=in_channels // 2, out_channels=in_channels, kernel_size=1, bias=False),
-                nn.BatchNorm2d(in_channels // 2),
-                nn.ReLU(),
-            ]
+            nn.Conv2d(in_channels=in_channels, out_channels=in_channels // 2, kernel_size=1, bias=False),
+            nn.BatchNorm2d(in_channels // 2),
+            nn.ReLU(),
+            MaskedConv2D(
+                mask_type="B",
+                in_channels=in_channels // 2,
+                out_channels=in_channels // 2,
+                kernel_size=3,
+                padding=1,
+                bias=False,
+            ),
+            nn.BatchNorm2d(in_channels // 2),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=in_channels // 2, out_channels=in_channels, kernel_size=1, bias=False),
+            nn.BatchNorm2d(in_channels),
+            nn.ReLU(),
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -100,22 +98,20 @@ class PixelCNN(nn.Module):
         super().__init__(*args, **kwargs)
 
         self.input_layer = nn.Sequential(
-            [
-                MaskedConv2D(
-                    mask_type="A", in_channels=1, out_channels=num_channels, kernel_size=7, padding=2, bias=False
-                ),
-                nn.BatchNorm2d(num_channels),
-                nn.ReLU(),
-            ]
+            MaskedConv2D(mask_type="A", in_channels=1, out_channels=num_channels, kernel_size=7, padding=3, bias=False),
+            nn.BatchNorm2d(num_channels),
+            nn.ReLU(),
         )
         self.layers = nn.Sequential(*[MaskedConvResidualBlock(in_channels=num_channels) for _ in range(num_layers)])
         self.last_conv = nn.Sequential(
-            MaskedConv2D(mask_type="B", in_channels=num_channels, out_channels=num_channels, kernerl_size=1),
+            MaskedConv2D(mask_type="B", in_channels=num_channels, out_channels=num_channels, kernel_size=1, bias=False),
             nn.ReLU(),
-            MaskedConv2D(mask_type="B", in_channels=num_channels, out_channels=num_channels, kernel_size=1),
+            MaskedConv2D(mask_type="B", in_channels=num_channels, out_channels=num_channels, kernel_size=1, bias=False),
             nn.ReLU(),
         )
-        self.out_layer = nn.Sequential(nn.Conv2d(in_channels=num_channels, out_channels=1, kernel_size=1), nn.Sigmoid())
+        self.out_layer = nn.Sequential(
+            nn.Conv2d(in_channels=num_channels, out_channels=1, kernel_size=1, bias=False), nn.Sigmoid()
+        )
 
     def forward(self, inputs: torch.Tensor) -> torch.Tensor:
         """feed fowrading pixel cnn.
---
+++ tests/test_models/test_autoregressive/test_autoregressive.py
@@ -0,0 +1,37 @@
+import pytest
+import torch
+
+from gen_ai.models.autoregressive import MaskedConv2D, MaskedConvResidualBlock, PixelCNN
+
+
+@pytest.mark.parametrize(
+    "mask_type, answer_mask", [("A", [[1, 1, 1], [1, 0, 0], [0, 0, 0]]), ("B", [[1, 1, 1], [1, 1, 0], [0, 0, 0]])]
+)
+def test_masked_conv_2d(mask_type, answer_mask):
+    masked_conv = MaskedConv2D(mask_type, in_channels=3, out_channels=3, kernel_size=3, padding=1)
+    answer_mask = torch.tensor(answer_mask).reshape(1, 1, 3, 3)
+
+    mask: torch.Tensor = masked_conv.mask
+
+    assert torch.Size([3, 3, 3, 3]) == mask.size()
+    assert torch.all(mask == answer_mask)
+
+
+def test_masked_conv_residual_block():
+    block = MaskedConvResidualBlock(3)
+
+    tmp_input = torch.randn(1, 3, 10, 10)
+
+    block_out = block(tmp_input)
+
+    assert block_out.size() == torch.Size([1, 3, 10, 10])
+
+
+def test_pixel_cnn():
+    pixel_cnn = PixelCNN(120, 5)
+
+    tmp_input = torch.randn(1, 1, 10, 10)
+
+    out = pixel_cnn(tmp_input)
+
+    assert out.size() == torch.Size([1, 1, 10, 10])
